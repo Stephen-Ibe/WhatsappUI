6e48d05db28f6a49f1300c4499aa0a0e7e5df9ac
Ibe Stephen <saintkalson@gmail.com>
2021-10-09 15:25:54 +0100

    added emoji icon

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -1,4 +1,6 @@
 import React from "react";
+import doubleCheck from "../assets/images/done_all.svg";
+import emojiIcon from "../assets/images/tag_faces.svg";
 
 function MainSection() {
   return (
@@ -24,6 +26,15 @@ function MainSection() {
         <div className="message sent">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla,
           officiis!
+          <div className="metadata">
+            <span className="date">05/20/2020</span>
+            <img src={doubleCheck} className="icon-small" alt="" />
+          </div>
+        </div>
+      </div>
+      <div className="chat-input-box">
+        <div className="icon emoji-selector">
+          <img src={emojiIcon} alt="" />
         </div>
       </div>
     </main>
