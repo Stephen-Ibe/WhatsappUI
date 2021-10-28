14d02a63f2befcf5a69d9f6a193dcf4aa2130c85
Ibe Stephen <saintkalson@gmail.com>
2021-10-28 05:26:53 +0100

    unresolved errors

--- src/components/subcomponents/Message.js
+++ src/components/subcomponents/Message.js
@@ -4,13 +4,14 @@ import doubleCheck from "../../assets/images/done_all.svg";
 function Message({ message }) {
   console.log(message);
   return (
-    <div className={`message ${message.isMainUser ? "sent" : "received"}`}>
-      {/* {message.msg} */}
-      <div className="metadata">
-        {/* <span className="date">{message.date.toLocaleString()}</span> */}
-        <img src={doubleCheck} className="icon-small" alt="" />
-      </div>
-    </div>
+    <></>
+    // <div className={`message ${message.isMainUser ? "sent" : "received"}`}>
+    //   {/* {message.msg} */}
+    //   <div className="metadata">
+    //     {/* <span className="date">{message.date.toLocaleString()}</span> */}
+    //     <img src={doubleCheck} className="icon-small" alt="" />
+    //   </div>
+    // </div>
   );
 }
 
